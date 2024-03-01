3881064df3d009181176eca198c589fc12fd35e0
RuddyMo <ruddymorel10@gmail.com>
2024-03-01 13:34:34 +0100

    feat<OrangeSoft>: add OrangeSoft components

---
+++ components/OrangeSoft.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="bg-elevationTwo rounded-primary py-[40px] px-[54px]">
+        <div class="flex justify-between">
+            <h1 class="text-hfour">Aventure avec OrangeSoft depuis 5 ans</h1>
+            <button class="i-mdi-arrow-top-right text-secondary text-xl"></button>
+        </div>
+        <h2 class="text-secondary text-body">21 projets réalisés 🎉
+        </h2>
+        <div class="mt-4 flex justify-between max-lg:grid max-lg:grid-cols-2 max-lg:gap-4">
+            <div class="flex flex-col gap-1 items-center">
+                <img src="../assets/Bros/Bros.png" alt="" class="w-[80px]">
+                <p class="text-body font-extralight">Mr. Hashtag</p>
+            </div>
+            <div class="flex flex-col gap-1 items-center">
+                <img src="../assets/Bros/Bros-1.png" alt="" class="w-[80px]">
+                <p class="text-body font-extralight">Energy Saver</p>
+            </div>
+            <div class="flex flex-col gap-1 items-center">
+                <img src="../assets/Bros/Bros-2.png" alt="" class="w-[80px]">
+                <p class="text-body font-extralight">Online School</p>
+            </div>
+            <div class="flex flex-col gap-1 items-center">
+                <img src="../assets/Bros/Bros-3.png" alt="" class="w-[80px]">
+                <p class="text-body font-extralight">Covid Pass</p>
+            </div>
+        </div>
+    </div>
+</template>
